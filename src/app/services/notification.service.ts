e5fb0bb87d894be93b0bce76dbad9a06c0ae59c8
Evelyn-CHEN2 <cyu86220@gmail.com>
2025-08-25 23:12:08 +1000

    Modified code in groups.html to add condition for editing, deleting groups and channels of admin self.

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { User } from '../interface';
+import { Observable } from 'rxjs';
 import { Notification } from '../interface';
 
 @Injectable({
