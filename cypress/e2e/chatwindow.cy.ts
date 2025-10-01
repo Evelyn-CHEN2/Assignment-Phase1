30252f2055f43061974dfbff96df4c0a1ac7f684
Evelyn-CHEN2 <cyu86220@gmail.com>
2025-10-01 10:51:51 +1000

    Added mock backend for banUser to void displaying errMsg after testing completion.

--- cypress/e2e/chatwindow.cy.ts
+++ cypress/e2e/chatwindow.cy.ts
@@ -1,24 +1,44 @@
 describe('Chatwindow', () => {
   const route = '/chatwindow/c11';
+  const currentUser = { _id: 'u1', username: 'tom', email: 'tom@com', groups: ['g1', 'g2'] };
   beforeEach(() => {
-    cy.visit(route, {
-      onBeforeLoad(win) {
-        win.localStorage.setItem('currentUser', JSON.stringify({ _id: 'u1', isSuper: true }));
-      }
-    });
     // Mock membership
     cy.intercept('GET', '**/api/fetchmembership*', {
       statusCode: 200,
-      body: { _id: 'm1', role: 'chatuser', admin: 'u1', groups: ['g1', 'g2']}
-    });
-    // Mock chat message
+      body: { _id: 'm1', role: 'admin', admin: 'u1', groups: ['g1', 'g2']}
+    }).as('fetchMembership');
+
+    cy.intercept('GET', '**/api/allchannels', {
+      statusCode: 200,
+      body: [
+        { _id: 'c11', channelname: 'general', groupId: 'g1', chatMsg: [] },
+        { _id: 'c12', channelname: 'examples', groupId: 'g1', chatMsg: [] },
+      ],
+    }).as('getChannels');
+
+    cy.intercept('GET', '**/api/fetchallusers', {
+      statusCode: 200,
+      body: [
+        { _id: 'u1', username: 'tom', avatar: 'data:image/png;base64,xxx', isSuper: false },
+        { _id: 'u2', username: 'jerry', avatar: 'data:image/png;base64,xxx', isSuper: false },
+      ],
+    }).as('getUsers');
+
     cy.intercept('GET', '**/api/fetchchatmessages/c11', {
       statusCode: 200,
       body: [
         { _id: 'm1', sender: 'u2', message: 'Hello!', timestamp: new Date().toISOString() },
         { _id: 'm2', sender: 'u1', message: 'Hi!', timestamp: new Date().toISOString() }
       ]
-    }); 
+    }).as('fetchMsgs'); 
+
+    cy.visit(route, {
+      onBeforeLoad(win) {
+        win.localStorage.setItem('currentUser', JSON.stringify({ _id: 'u1', isSuper: true }));
+      }
+    });
+
+    cy.wait(['@fetchMembership', '@getChannels', '@getUsers', '@fetchMsgs']);
   });
 
   // Go back to last page
@@ -32,21 +52,22 @@ describe('Chatwindow', () => {
     cy.get('.msg-bubble .msg-text').first().should('contain.text', 'Hello');
   });
 
-  it('should send a message', () => {
+  it('should send a message and display it', () => {
     cy.get('input[name="message"]').type('Hi!');
     cy.get('form.chat-input').submit();
     cy.get('.msg-bubble .msg-text').last().should('contain.text', 'Hi!')  // contain.text equals to include.text, have.text is strict match
   });
 
   it('should open ban modal when clicking user avatar', () => {
+    cy.intercept('POST', '**/api/banuserbyID/*', { statusCode: 200, body: {}}).as('banUser');
     // Open modal
-    cy.get('.msg-avatar').first().click();
+    cy.get('.msg-avatar').should('exist').first().click();
     cy.get('#confirmBanModal').should('be.visible');
     cy.get('#confirmBanLabel').should('contain.text', 'Are you sure to ban');
 
     // Confirm ban
     cy.contains('button', 'Ban').click();
-
+    cy.wait('@banUser');
     // Assert modal closes
     cy.get('#confirmBanModal').should('not.be.visible');
   });
